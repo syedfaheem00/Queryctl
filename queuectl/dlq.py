957949d5bb65f29b7a7b633e96e124a610a13697
syedfaheem00 <syedfaheem3724@gmail.com>
2025-11-08 14:00:26 +0530

    Update dlq.py

--- queuectl/dlq.py
+++ queuectl/dlq.py
@@ -1,1 +1,27 @@
-<DLQ logic>
+from queuectl.storage import JobStorage
+
+class DLQManager:
+    def __init__(self, storage: JobStorage):
+        self.storage = storage
+
+    def add_job(self, job_dict):
+        dlq = self.storage.load_dlq()
+        dlq.append(job_dict)
+        self.storage.save_dlq(dlq)
+
+    def retry_job(self, job_id):
+        dlq = self.storage.load_dlq()
+        jobs = self.storage.load_jobs()
+        for job in dlq:
+            if job["id"] == job_id:
+                job["state"] = "pending"
+                job["attempts"] = 0
+                jobs.append(job)
+                dlq.remove(job)
+                self.storage.save_jobs(jobs)
+                self.storage.save_dlq(dlq)
+                return True
+        return False
+
+    def list_dlq(self):
+        return self.storage.load_dlq()
