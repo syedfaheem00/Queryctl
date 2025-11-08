dfe4b33e082f4ef63be5c3d708dccf009de4ed0f
syedfaheem00 <syedfaheem3724@gmail.com>
2025-11-08 13:59:52 +0530

    Update config.py

--- queuectl/config.py
+++ queuectl/config.py
@@ -1,1 +1,32 @@
-<configuration management>
+import json
+import threading
+
+_CONFIG_FILE = "config.json"
+_DEFAULTS = {"max_retries": 3, "backoff_base": 2, "worker_count": 1}
+
+class Config:
+    def __init__(self, path=_CONFIG_FILE):
+        self.path = path
+        self.lock = threading.Lock()
+        self.data = _DEFAULTS.copy()
+        self.load()
+
+    def load(self):
+        with self.lock:
+            try:
+                with open(self.path, "r") as f:
+                    self.data.update(json.load(f))
+            except Exception:
+                pass
+        return self.data
+
+    def save(self):
+        with self.lock, open(self.path, "w") as f:
+            json.dump(self.data, f, indent=2)
+
+    def set(self, key, value):
+        self.data[key] = int(value) if value.isdigit() else value
+        self.save()
+
+    def get(self, key, default=None):
+        return self.data.get(key, default)
