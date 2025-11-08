4bb4ef4fdd810721c3194443be63a9481a6b00e8
syedfaheem00 <syedfaheem3724@gmail.com>
2025-11-08 14:02:29 +0530

    Update utils.py

--- queuectl/utils.py
+++ queuectl/utils.py
@@ -1,1 +1,9 @@
-<helpers>
+import time
+
+def compute_exponential_backoff(base, attempts):
+    return base ** attempts
+
+def sleep_backoff(base, attempts):
+    delay = compute_exponential_backoff(base, attempts)
+    print(f"[Backoff] Sleeping for {delay} seconds before retry...")
+    time.sleep(delay)
